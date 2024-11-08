4525606510785c6a485560eebdef60285d0e8568
MiguelAngelBusto <chetobustos@gmail.com>
2024-11-08 19:13:13 -0300

    Terminado Vista de Productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -4,12 +4,14 @@ using tl2_tp6_2024_MiguelAngelBusto.Models;
 using System.Collections.Generic;
 using System.Linq;
 using tl2_tp6_2024_MiguelAngelBusto.Repositorios;
+using tl2_tp6_2024_MiguelAngelBusto.Models;
 
 namespace tl2_tp6_2024_MiguelAngelBusto.Controllers;
 
 public class ProductosController : Controller {
 
     private ProductosRepository repositorio;
+
 
     public ProductosController (){
         repositorio = new ProductosRepository();
@@ -20,6 +22,34 @@ public class ProductosController : Controller {
         return View(repositorio.GetProductos());
     }
 
+    [HttpGet]
+    public IActionResult Crear(){
+        return View(new Productos());
+    }
+
+    [HttpPost]
+    public IActionResult Crear(Productos aux){
+        repositorio.Create(aux);
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult EditarProducto(int idProducto)
+    {  
+        Productos prod;
+        prod = repositorio.GetByID(idProducto);
+        return View(prod);
+    }
 
 
+    [HttpPost]
+    public IActionResult EditarProducto(Productos item)
+    {   
+        repositorio.Update(item.IdProducto,item);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult DeleteProducto(int idProducto){
+        repositorio.Delete(idProducto);
+        return RedirectToAction("Index");
+    }
 }
